e812ffca92f0b44fcfc105c0a230f2c1f3d63320
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-08-17 23:29:30 +0300

    chore: copy the contents of alx-listing-app-00 to alx-listing-app-03

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+// import Image from "next/image";
+// import { Geist, Geist_Mono } from "next/font/google";
+import Pill from "@/components/common/Pill";
+
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+
+
+const Home:React.FC = () => {
+  return (
+    <main>
+      <Pill />
+      Home
+      
+    </main>
+  )
+}
+
+
+export default Home;
